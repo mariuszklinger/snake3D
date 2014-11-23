e7a1a490195d71dd7cef6315c3a0f11e07a198b0
Mariusz Klinger <mariusz@mariuszklinger.pl>
2014-11-23 23:16:10 +0000

    common THREE objects moved to Engine class

--- src/js/client/Game.js
+++ src/js/client/Game.js
@@ -1,6 +1,6 @@
 'use strict';
 
-define(['core/Engine', 'core/Snake', 'core/SETTINGS', 'THREE.TrackballControls'], 
+define(['core/Engine', 'core/Snake', 'core/SETTINGS'], 
     function(Engine, Snake, SETTINGS){
 
     function Game(){
@@ -8,100 +8,15 @@ define(['core/Engine', 'core/Snake', 'core/SETTINGS', 'THREE.TrackballControls']
         this.player = new Snake();
 
         this.engine.addSnake(this.player);
-
-        this.init();
     };
 
-    Game.prototype = {        
+    Game.prototype = {
         
         keyPress: function(event){
         },
 
-        init: function(){
-
-            var self = this;
-
-            var renderer = new THREE.WebGLRenderer();
-            renderer.setSize( 1450, 800 );
-            renderer.setClearColor( 0xCCCCCC, 1);
-            document.body.appendChild( renderer.domElement );
-
-            var camera = new THREE.PerspectiveCamera(
-                35,             // Field of view
-                1450 / 800,     // Aspect ratio
-                0.1,            // Near plane
-                10000           // Far plane
-            );
-
-            camera.position.set(20, 0, 100);
-            camera.lookAt(new THREE.Vector3(20, 0, 100));    
-            
-            this.camera = camera; 
-
-            this.renderer = renderer;   
-
-            var scene = new THREE.Scene();
-            //scene.fog = new THREE.FogExp2( 0xcccccc, 0.01 );
-
-            this.scene = scene;
-
-            var geometry = new THREE.BoxGeometry( 5, 5, 5 );
-            var material = new THREE.MeshLambertMaterial( { color: 0xBDB9AF } );
-            var mesh;
-
-            for (var i = 1; i >= 0; i--) {
-                mesh = new THREE.Mesh( geometry, material );
-                mesh.position.x = -50 + Math.random() * 100;
-                mesh.position.y = -50 + Math.random() * 100;
-                mesh.position.z = -50 + Math.random() * 100;
-                scene.add( mesh );
-            };
-            
-            var createMesh = function(){
-                var mesh = new THREE.Mesh( geometry, material );
-                var material = new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } );
-                mesh.position.x = -50 + Math.random() * 100;
-                mesh.position.y = -50 + Math.random() * 100;
-                mesh.position.z = -50 + Math.random() * 100;
-                scene.add( mesh );
-            }
-            setInterval(createMesh, 1000);
-
-            var light = new THREE.DirectionalLight( 0xffffff );
-            light.position.set( 1, 1, 1 );
-            scene.add( light );
-
-            light = new THREE.DirectionalLight( 0x002288 );
-            light.position.set( -1, -1, -1 );
-            scene.add( light );
-
-            light = new THREE.DirectionalLight( 0xED23AF );
-            light.position.set( 0, -20, 1 );
-            scene.add( light );
-
-            var controls = new THREE.TrackballControls(camera);
-
-            controls.rotateSpeed = 1.0;
-            controls.zoomSpeed = 1.2;
-            controls.panSpeed = 0.8;
-
-            controls.noZoom = false;
-            controls.noPan = false;
-
-            controls.staticMoving = true;
-            controls.dynamicDampingFactor = 0.3;
-
-            controls.keys = [ 65, 83, 68 ];
-
-            controls.addEventListener('change', function(){
-                self.render.call(self);
-            });
-
-            this.controls = controls;
-        },
-
         render: function(){
-            this.renderer.render(this.scene, this.camera);
+            this.engine.grid.renderer.render(this.engine.grid.scene, this.engine.grid.camera);
         },
 
         animate: function(){
@@ -110,7 +25,7 @@ define(['core/Engine', 'core/Snake', 'core/SETTINGS', 'THREE.TrackballControls']
 
             var _animate = function(){
                 requestAnimationFrame(_animate);
-                self.controls.update();
+                self.engine.grid.controls.update();
                 self.render();
             }
 
--- src/js/core/Grid.js
+++ src/js/core/Grid.js
@@ -1,12 +1,83 @@
 'use strict';
 
-define(['THREE', 'core/SETTINGS'], function(THREE, SETTINGS){
+define(['core/SETTINGS', 'THREE.TrackballControls'], function(SETTINGS){
 
     function Grid(){
-    
+        this.init();
     };
 
     Grid.prototype = {
+
+        scene: null,
+        camera: null,
+        renderer: null,
+
+        init: function(){
+
+            this.renderer = (function(THREE){
+                var renderer = new THREE.WebGLRenderer();
+                renderer.setSize( 1450, 800 );
+                renderer.setClearColor( 0xCCCCCC, 1);
+                document.body.appendChild(renderer.domElement);
+            
+                return renderer;
+            })(THREE);
+
+            this.camera = (function(){
+                var camera = new THREE.PerspectiveCamera(
+                    35,             // Field of view
+                    1450 / 800,     // Aspect ratio
+                    0.1,            // Near plane
+                    10000           // Far plane
+                );
+
+                camera.position.set(20, 0, 100);
+                camera.lookAt(new THREE.Vector3(20, 0, 100));
+                return camera;         
+            })(); 
+
+            this.scene = new THREE.Scene();
+
+            this.controls = (function(self){
+                var controls = new THREE.TrackballControls(self.camera);
+
+                controls.rotateSpeed = 1.0;
+                controls.zoomSpeed = 1.2;
+                controls.panSpeed = 0.8;
+
+                controls.noZoom = false;
+                controls.noPan = false;
+
+                controls.staticMoving = true;
+                controls.dynamicDampingFactor = 0.3;
+
+                controls.keys = [ 65, 83, 68 ];
+
+                controls.addEventListener('change', function(){
+                    self.renderer.render.call(self);
+                });
+
+                return controls;
+            })(this);
+
+            // --- D E M O ---
+            var geometry = new THREE.BoxGeometry( 5, 5, 5 );
+            var material = new THREE.MeshLambertMaterial( { color: 0xBDB9AF } );
+            var mesh;
+            var self = this;
+
+            var createMesh = function(){
+                var mesh = new THREE.Mesh( geometry, material );
+                var material = new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } );
+                mesh.position.x = -50 + Math.random() * 100;
+                mesh.position.y = -50 + Math.random() * 100;
+                mesh.position.z = -50 + Math.random() * 100;
+                self.scene.add( mesh );
+            }
+
+            setInterval(createMesh, 1000);
+            // --- CUT HERE ---
+        },
 
         putBlock: function(vector){
         },
