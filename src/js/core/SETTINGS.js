21446761a57ce231301da9348b11ffdc0c68cf36
Mariusz Klinger <mariusz@mariuszklinger.pl>
2014-12-06 23:22:11 +0000

    updated SETTINGS file

--- src/js/core/SETTINGS.js
+++ src/js/core/SETTINGS.js
@@ -5,9 +5,9 @@ define([], function(){
     var SETTINGS = {
         
         // width, height and deep
-        GRID_DIMENSIONS: [15, 15, 15],
+        GRID_DIMENSIONS: [10, 10, 10],
         
-        CUBE_SIZE: 40,
+        CUBE_SIZE: 5,
     }
 
     return SETTINGS;
