54f93194aad538d3c0c7e5be4bd0be48e85740c3
Mariusz Klinger <mariusz@mariuszklinger.pl>
2015-01-09 23:36:13 +0000

    added random color assigned to whole Snake

--- src/js/core/Snake.js
+++ src/js/core/Snake.js
@@ -5,14 +5,13 @@ define(['core/SETTINGS'], function(SETTINGS){
     function Snake(scene){
         
         var cs = SETTINGS.CUBE_SIZE;
+        this.color = Math.random() * 0xFFFFFF;
 
         this.body = [
             {v:[cs/2, cs/2, cs/2 + 1 * cs]},
             {v:[cs/2, cs/2, cs/2 + 2 * cs]},
             {v:[cs/2, cs/2, cs/2 + 3 * cs]},
             {v:[cs/2, cs/2, cs/2 + 4 * cs]},
-            {v:[cs/2, cs/2, cs/2 + 5 * cs]},
-            {v:[cs/2, cs/2, cs/2 + 6 * cs]},
         ];
     };
 
